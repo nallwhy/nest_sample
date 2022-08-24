7828fa6fb5a337280238e053cc67ba95db9d2905
Jinkyou Son <nallwhy@gmail.com>
2022-08-24 20:19:08 +0900

    Create AccountsService

--- src/web/controllers/users.controller.ts
+++ src/web/controllers/users.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { AccountsService } from 'src/domain/accounts/accounts.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly accountsService: AccountsService) {}
+
   @Get()
   listUsers(): [] {
-    return [];
+    return this.accountsService.listUsers();
   }
 }
