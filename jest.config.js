8bfff50f3d069cfa2898019662bff0ccc5ec16a5
Jinkyou Son <nallwhy@gmail.com>
2023-11-14 18:48:18 +0900

    Fix problem

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
+/* eslint-disable */
+const fs = require('fs');
+
+const config = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, 'utf-8'));
+
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
   rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': ['@swc/jest', { ...config }],
   },
   collectCoverageFrom: ['**/*.(t|j)sx?'],
   coverageDirectory: '../coverage',
